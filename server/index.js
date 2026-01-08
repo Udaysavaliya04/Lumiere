aa30095522e497352e5794ae75a5d58981741429
UdaySavaliya <udaysavaliya04@gmail.com>
2026-01-09 00:40:59 +0530

    init

--- server/index.js
+++ server/index.js
@@ -22,9 +22,19 @@ const productRoutes = require('./routes/productRoutes');
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
